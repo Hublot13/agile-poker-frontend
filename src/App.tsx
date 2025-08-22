4fc335e3776e04c7ee4d12f0b83d4645f056fc33
Hublot13 <98663196+Hublot13@users.noreply.github.com>
2025-08-22 18:44:14 +0530

    AP- Added notification context (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ import { HomePage } from './pages/HomePage';
 import { CreateRoomPage } from './pages/CreateRoomPage';
 import { JoinRoomPage } from './pages/JoinRoomPage';
 import { PlanningRoomPage } from './pages/PlanningRoomPage';
+import { NotificationProvider } from './contexts/NotficationContext';
 
 function App() {
   return (
     <ThemeProvider>
       <SocketProvider>
-        <Router>
-          <div className="App">
-            <ConnectionStatus />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/create" element={<CreateRoomPage />} />
-              <Route path="/join" element={<JoinRoomPage />} />
-              <Route path="/join/:roomCode" element={<JoinRoomPage />} />
-              <Route path="/room/:roomCode" element={<PlanningRoomPage />} />
-            </Routes>
-          </div>
-        </Router>
+        <NotificationProvider>
+          <Router>
+            <div className="App">
+              <ConnectionStatus />
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/create" element={<CreateRoomPage />} />
+                <Route path="/join" element={<JoinRoomPage />} />
+                <Route path="/join/:roomCode" element={<JoinRoomPage />} />
+                <Route path="/room/:roomCode" element={<PlanningRoomPage />} />
+              </Routes>
+            </div>
+          </Router>
+        </NotificationProvider>
       </SocketProvider>
     </ThemeProvider>
   );
